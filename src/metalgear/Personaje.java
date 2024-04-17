54e8267c7b18b97b767965e9deee377629c6d2af
jose <gregorio.jr2@gmail.com>
2024-04-17 10:04:07 +0100

    comienzo+

--- src/metalgear/Personaje.java
+++ src/metalgear/Personaje.java
@@ -1,13 +1,1 @@
 package metalgear;
-public class Personaje {
-    String nombre;
-    int edad;
-
-    void presentarse(){
-        System.out.println("hola soy "+nombre);
-    }
-    public Personaje(String nombre, int edad){
-        this.nombre=nombre;
-        this.edad=edad+1;
-    }
-}
